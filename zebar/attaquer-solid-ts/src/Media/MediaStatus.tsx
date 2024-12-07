37808b4b26aaafd1b46e564db4840c77dfa07329
unknown <iAttaquer@proton.me>
2024-12-07 14:12:59 +0100

    chore: updated media status. Added play-pause, prev, next buttons

--- zebar/attaquer-solid-ts/src/Media/MediaStatus.tsx
+++ zebar/attaquer-solid-ts/src/Media/MediaStatus.tsx
@@ -7,17 +7,56 @@ interface MediaStatusProps {
 }
 
 const MediaStatus: Component<MediaStatusProps> = (props) => {
+  const MediaIcons = {
+    media: <img
+            src="./assets/icons/icons8-musical-note-32.png"
+            height={23}
+            width={23}
+          />,
+    Brave: <img
+            src="./assets/icons/icons8-brave-32.png"
+            height={23}
+            width={23}
+           />,
+    'Spotify.exe': <img src="./assets/icons/icons8-spotify-32.png"
+            height={23}
+            width={23}
+           />,
+  }
   return (
     <>
       {props.media?.session && (
         <div class={`template media`}>
-          <img
-            src="./assets/icons/icons8-musical-note-32.png"
-            height="23"
-            width="23"
-          />
-            {props.media?.session?.title} | 
-            {props.media?.session?.artist}
+          {MediaIcons[props.media?.currentSession.sessionId] ?? MediaIcons['media']}
+            <button class="prev"
+              onClick={() => {
+                props.media?.previous();
+              }}>
+                <span class="content">
+                  󰒮
+                </span>
+            </button>
+            <button class="play-pause"
+              onClick={() => {
+                props.media?.togglePlayPause();
+                console.log(props.media?.allSessions);
+              }}>
+                <span class="content">
+                  {props.media?.currentSession.isPlaying ? "󰏤" : "󰐊"}
+                </span>
+            </button>
+            <button class="next"
+              onClick={() => {
+                props.media?.next();
+              }}>
+                <span class="content">
+                  󰒭
+                </span>
+            </button>
+            <div class="media-text">
+              {props.media?.session?.title} | 
+              {props.media?.session?.artist}
+            </div>
           <div class="progress-play" style={`width: ${props.media?.session?.position/props.media?.session?.endTime*100}%`}></div>
         </div>
       )}
